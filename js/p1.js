56ef18b57ed656408ec162ffd0727da4c36fb17e
Jakeeeeeeeeeeeee <yangjinchen1995@gmail.com>
2021-01-24 01:06:01 +1100

    1.24 update

--- js/p1.js
+++ js/p1.js
@@ -66,3 +66,12 @@ function initMap() {
      zoom: 8,
    });
  }
+
+ function myFunction() {
+  var x = document.getElementById("nav");
+  if (x.className === "nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav";
+  }
+}
